89e289479f9de7a5efc98a3a2fe5d5ca6bbf5d39
Antoan Angelov <antoan.angelov@gmail.com>
2015-07-14 00:45:46 +0300

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="spaceship",
-    version="0.8",
+    version="0.8.1",
     packages=find_packages(),
     install_requires=[
         'netifaces'
